ae3d397c72b772d1f92d5f8802e5df98f75c5248
Nikolas Schildberg <nikolas.schildberg@gmail.com>
2022-05-08 23:42:16 -0300

    Finish implement frontend v1 (fake form submit)
    - Implement screenshot feature on content step, including styles and
    animations
    - Implement Feedback submussion success page (form still not really
    submitting, since there'e no backend yet)

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -39,17 +40,26 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
     const [feedbackType,setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleResetFeedbackType(){
         setFeedbackType(null)
+        setFeedbackSent(false)
     }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">            
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-            ):
-                <FeedbackContentStep feedbackType={feedbackType} onBackClicked={handleResetFeedbackType}/>}
+            {
+                feedbackSent? <FeedbackSuccessStep onRestartFeedback={ handleResetFeedbackType}/> :
+                !feedbackType ? (
+                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+                ):
+                    <FeedbackContentStep
+                        feedbackType={feedbackType}
+                        onBackClicked={handleResetFeedbackType}
+                        onFeedbackSent = {() => setFeedbackSent(true)}
+                    />
+            }
 
             <footer className="text-xs text-neutral-400">
                 Feito com 💜 por <a className='underline underline-offset-1' href='https://github.com/NikolasSchildberg'>Nikolas Schildberg</a>
--- src/components/WidgetForm/steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/steps/FeedbackContentStep.tsx
@@ -1,20 +1,36 @@
-import { ArrowLeft, Camera } from "phosphor-react";
-import { Fragment } from "react"
+import { ArrowLeft } from "phosphor-react";
+import { FormEvent, Fragment, useState } from "react"
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenshotButton } from "../../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
     onBackClicked: () => void;
+    onFeedbackSent: () => void;
 }
 
 export function FeedbackContentStep({
     feedbackType,
-    onBackClicked
+    onBackClicked,
+    onFeedbackSent
 }: FeedbackContentStepProps) {
     
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState('');
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     
+    function handleSubmitFeedback(event: FormEvent){
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment
+        })
+
+        onFeedbackSent()
+    }
+
     return (
         <Fragment>                            
             
@@ -40,33 +56,32 @@ export function FeedbackContentStep({
             
             </header>
             
-            <form className="my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
             
                 <textarea
                     className="min-w-[304px] w-full h-min-[112px] text-sm text-zinc-100 placeholder-zinc-400
                         bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1
-                        resize-none border-2 border-zinc-600 focus:outline-none scrollbar 
+                        resize-none border-2 border-zinc-600 focus:outline-none
                         scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
                 />
             
                 <footer className="flex gap-2 mt-2">
                     
-                    <button
-                        type='button'
-                        className="p-2 bg-zinc-800 rounded-md border-transparent 
-                        hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 
-                        focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
-                        >
-                        <Camera className="w-6 h-6 text-zinc-100"/>
-                    </button>
+                    <ScreenshotButton
+                        screenshot = {screenshot}
+                        onScreenshotTook = {setScreenshot}
+                    />
 
                     <button
                         type='submit'
+                        disabled={comment.length === 0}
                         className="p-2 bg-brand-500 rounded-sm border-transparent
                         flex-1 flex justify-center items-center text-sm hover:bg-brand-300
                         focus:outline-none focus:ring-2 focus:ring-offset-2 
-                        focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors
+                        disabled:opacity-50 disabled:hover:bg-brand-500"
                         >
                         Enviar Feedback
                     </button>
